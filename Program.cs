65d19c7d49e598ac156ba38bef388cac636a9803
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-12 15:04:40 -0700

    string builder and how to make better cleaner code

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderFundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder("HELLO WORLD");
+            //builder.Append('-', 10);
+            //builder.AppendLine();
+            //builder.Append("Header");
+
+            //builder.AppendLine();
+
+
+            //builder.Append('-', 10);
+            //builder.AppendLine();
+            //builder.Append("Header");
+
+            //builder.AppendLine();
+
+            //builder.Append('-', 10);
+            //builder.AppendLine();
+            //builder.Append("Header");
+
+            //builder.Replace('-', '+');
+
+            //builder.Remove(0, 10);
+
+            //builder.Insert(0, new string('-', 10));
+
+            ////THIS CODE BELOW MAKES CODE LOOK WAY CLEANER FROM THE TOP
+
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("HEADER")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First char: " + builder[0]);
+        }
+    }
+}
